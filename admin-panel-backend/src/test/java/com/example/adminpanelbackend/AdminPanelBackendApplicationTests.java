0cb13c34963cc2f19e65ac44b01c0005af6c1cf3
eteixeira <enzo.teixeira@etu.univ-grenoble-alpes.fr>
2024-11-24 14:12:49 +0100

    Initial commit

---
+++ admin-panel-backend/src/test/java/com/example/adminpanelbackend/AdminPanelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adminpanelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPanelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
